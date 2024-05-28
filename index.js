dc221577a126a6c034de74a3ba7f1caf248a95d5
Dayane Floriani Antunes <36275146+dfloriani@users.noreply.github.com>
2024-05-28 19:27:01 -0300

    playing with using server

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-document.getElementById("htmlcss").onclick = function () {
-    location.href = "https://dfloriani.github.io/htmlandcss.html"
-};
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+  
+  app.use(express.static("public"));
+
+  app.get("/", (req, res) => {
+    res.render("index.ejs")
+  });
+
+  app.get("/htmlandcss", (req, res) => {
+    res.render("htmlandcss.ejs")
+  });
